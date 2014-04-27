3337def0161bb5633a4a5d9ecb1bced77b211213
Tom Kiefhaber <tom.kiefhaber@livingsocial.com>
2014-04-27 11:16:48 -0500

    users create is working, but sessions are fucked now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,19 +4,22 @@ class UsersController < ApplicationController
   end
 
   def create
-    p 'what?'
     if @user = User.where(username: params[:user][:username].strip).present?
-      p 'dinosaur'
+      flash[:notice] = "You already exist, sign in, #{@user.username}"
       redirect_to signin_path
     else
-      p 'sandwich'
-      if @user = User.create(params[:user])
-        p 'nuke'
+      if @user = User.create(user_params)
+        flash[:notice] = "We've created your account, now sign in as #{@user.username}"
         redirect_to signin_path
       else
-        p 'baaaack'
         redirect_to back
       end
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 end
