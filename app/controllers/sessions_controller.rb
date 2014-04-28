283c5036e149a14d20d5bfc3ce39290b58c02c7d
Tom Kiefhaber <tom.kiefhaber@livingsocial.com>
2014-04-28 09:36:24 -0400

    user_id still not being set in session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,9 @@ class SessionsController < ApplicationController
     user = User.where(username: params[:signin][:username].strip).first
 
     if user && user.authenticate(params[:signin][:password])
-      session[:user_id] = user.id 
-      redirect_to root_path, notice: "Signed in successfully."
+      session[:user_id] = user.id
+      flash[:notice] = "Signed in successfully."
+      redirect_to root_path
     else
       flash[:error] = "Wrong username/password."
       render :new
